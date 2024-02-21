4e1eb38860cc00904f7ae2ef4638396d8f9c1437
Gibran Malheiros <gibatronic@gmail.com>
2024-02-21 14:51:36 +0100

    improved bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,43 @@
+import { INestApplication, Logger } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { NestExpressApplication } from '@nestjs/platform-express'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { AppModule } from './app.module'
 
-async function bootstrap() {
+async function bootstrap(routePrefix: string, title: string) {
+    const app = await createApp(routePrefix)
+    const logger = new Logger('bootstrap')
+
+    setupSwagger(app, routePrefix, title)
+    await app.listen(3000)
+
+    logger.log(`Serving at ${await app.getUrl()}`)
+}
+
+async function createApp(routePrefix: string) {
     const app = await NestFactory.create<NestExpressApplication>(AppModule)
 
     app.disable('x-powered-by')
-    app.setGlobalPrefix('yak-shop')
+    app.setGlobalPrefix(routePrefix)
 
+    return app
+}
+
+function setupSwagger(
+    app: INestApplication,
+    routePrefix: string,
+    title: string,
+) {
     const swaggerConfig = new DocumentBuilder()
-        .setTitle('YakShop')
-        .setExternalDoc('OpenAPI JSON', 'yak-shop-json')
+        .setTitle(title)
+        .setExternalDoc('OpenAPI JSON', `${routePrefix}-json`)
         .build()
 
     const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig)
 
-    SwaggerModule.setup('yak-shop', app, swaggerDocument, {
-        customSiteTitle: 'YakShop',
+    SwaggerModule.setup(routePrefix, app, swaggerDocument, {
+        customSiteTitle: title,
     })
-
-    await app.listen(3000)
 }
 
-bootstrap()
+bootstrap('yak-shop', 'YakShop')
